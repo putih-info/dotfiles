cc2e640096da737787699c82ef8008860b6fd199
putih-info <putih.info@gmail.com>
2017-06-29 19:07:54 +0900

    First Commit

---
+++ make_ln.sh
@@ -0,0 +1,17 @@
+ln -s dotfiles/git/gitconfig ~/.gitconfig
+ln -s dotfiles/git/gitignore_global ~/.gitignore_global
+
+ln -s dotfiles/zsh/zshrc ~/.zshrc
+ln -s dotfiles/zsh/zshrc.aliases ~/.zshrc.aliases
+ln -s dotfiles/zsh/zshrc.functions ~/.zshrc.functions
+ln -s dotfiles/zsh/zshrc.darwin ~/.zshrc.darwin
+ln -s dotfiles/zsh/zshrc.linux ~/.zshrc.linux
+ln -s dotfiles/zsh/zshenv ~/.zshenv
+ln -s dotfiles/zsh/zsh_history ~/.zsh_history
+
+ln -s dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+ln -s dotfiles/vim/vimrc ~/.vimrc
+ln -s dotfiles/vim/gvimrc ~/.gvimrc
+
+
