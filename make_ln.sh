de0ccfd0f480e9b3fbadad4f566693f1e0927772
putih-info <putih.info@gmail.com>
2017-07-06 14:14:15 +0900

    Update

--- make_ln.sh
+++ make_ln.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-ln -s dotfiles/git/gitconfig ~/.gitconfig
-ln -s dotfiles/git/gitignore_global ~/.gitignore_global
+ln -si dotfiles/git/gitconfig ~/.gitconfig
+ln -si dotfiles/git/gitignore_global ~/.gitignore_global
 
-ln -s dotfiles/zsh/zshrc ~/.zshrc
-ln -s dotfiles/zsh/zshrc.aliases ~/.zshrc.aliases
-ln -s dotfiles/zsh/zshrc.functions ~/.zshrc.functions
-ln -s dotfiles/zsh/zshrc.darwin ~/.zshrc.darwin
-ln -s dotfiles/zsh/zshrc.linux ~/.zshrc.linux
-ln -s dotfiles/zsh/zshenv ~/.zshenv
-ln -s dotfiles/zsh/zsh_history ~/.zsh_history
+ln -si dotfiles/zsh/zshrc ~/.zshrc
+ln -si dotfiles/zsh/zshrc.aliases ~/.zshrc.aliases
+ln -si dotfiles/zsh/zshrc.functions ~/.zshrc.functions
+ln -si dotfiles/zsh/zshrc.darwin ~/.zshrc.darwin
+ln -si dotfiles/zsh/zshrc.linux ~/.zshrc.linux
+ln -si dotfiles/zsh/zshenv ~/.zshenv
+ln -si dotfiles/zsh/zsh_history ~/.zsh_history
 
-ln -s dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s dotfiles/tmux/tmux.conf.osx ~/.tmux.conf.osx
+ln -si dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -si dotfiles/tmux/tmux.conf.osx ~/.tmux.conf.osx
 
-ln -s dotfiles/vim/vimrc ~/.vimrc
-ln -s dotfiles/vim/gvimrc ~/.gvimrc
+ln -si dotfiles/vim/vimrc ~/.vimrc
+ln -si dotfiles/vim/gvimrc ~/.gvimrc
 
 
